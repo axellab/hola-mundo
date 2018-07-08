1d92bf8ac53d2e0a7b63d6386abbca7c12806336
AxelHub <axel.labruna@live.com.ar>
2018-07-07 21:17:10 -0300

    Correcciones generales

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,25 +1,24 @@
 package loom.hola_mundo;
 
-/**
- * Returns a String object that can be seen on the screen. 
- * <p>
+/** Returns a String object that can be seen on the screen. 
  * This method always returns a greeting, whether or not the 
  * method receives a name to say hello to.
- *
  * @author Axel
  * @version 1.0
  * @param  persona  a person name to say hello to
  * @return      the greeting to the person
- * @see         Image
- */
+ * @see         Image */
 public class HolaMundo {
-	
-	public String saludar (final String persona) {
+	/**Metodo para saludar
+	 **/
+	public String saludar(final String persona) {
 		
-		if (persona != "")
-			return "Hola, " + persona + "!";	/* retorno saludo personalizado */
-		else
+		if (persona != "") {
+			return "¡Hola, " + persona + "!";/*saludo gral*/
+		}
+		else {
 			return "Hola";	/*retorno saludo generico*/
+		}
 			
 	}
 }
